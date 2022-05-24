d5fda531ed2026cb33c113b8b9eccdc4612e6e4f
liangwangcm <liang.wang@covermore.com>
2022-05-24 15:05:33 +1000

    First

---
+++ go.mod
@@ -0,0 +1,12 @@
+module awesomeProject
+
+go 1.18
+
+require github.com/sideshow/apns2 v0.23.0
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20170512130425-ab89591268e0 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sideshow/apns2"
+	"github.com/sideshow/apns2/certificate"
+)
+
+func main() {
+
+	password := os.Args[1]
+	fmt.Println(password)
+
+	cert, err := certificate.FromP12File("../cert.p12", password)
+	if err != nil {
+		log.Fatal("Cert Error:", err)
+	}
+
+	notification := &apns2.Notification{}
+	notification.DeviceToken = "11aa01229f15f0f0c52029d8cf8cd0aeaf2365fe4cebc4af26cd6d76b7919ef7"
+	notification.Topic = "com.sideshow.Apns2"
+	notification.Payload = []byte(`{"aps":{"alert":"Hello!"}}`) // See Payload section below
+
+	// If you want to test push notifications for builds running directly from XCode (Development), use
+	// client := apns2.NewClient(cert).Development()
+	// For apps published to the app store or installed as an ad-hoc distribution use Production()
+
+	client := apns2.NewClient(cert).Production()
+	res, err := client.Push(notification)
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+}
